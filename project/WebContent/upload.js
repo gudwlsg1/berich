ed02c1d92d36f3afc67a079d948ab7229c07c008
gudwlsg1 <4587991@naver.com>
2017-12-16 03:16:37 +0900

    1
    1

--- project/WebContent/upload.js
+++ project/WebContent/upload.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
-	$("#validatebutton").attr('disabled', true);
-	
 	$("#browsebutton").on('click', function(){
 		$("#fileinput").trigger('click');
 	});
@@ -10,14 +8,5 @@ $(document).ready(function(){
 	});
 	$("#fileinput").on('change',function(){
 		  $("#filename").val($("#fileinput").val().replace(/C:\\fakepath\\/i, ''));
-		  var filenameFilter = /\.(xml)$/i;
-		  if($("#filename").val().length > 0 ){
-			  if($("#filename").val().match(filenameFilter))
-				  $("#validatebutton").attr('disabled', false);
-			  else
-				  alert("Select xml file");
-		  }else {
-			  $("#validatebutton").attr('disabled', true);
-		  }
 	});
 });
